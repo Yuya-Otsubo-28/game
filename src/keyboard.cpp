ff9a8b68676f7450a165971c54814c00bd0b4fc5
yotsubo <513047.otsubo@gmail.com>
2024-08-01 08:15:08 +0900

    modified: delete r key action

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -35,20 +35,6 @@ void keyboard(unsigned char key, int x, int y)
 		  }
 	  
 	  break;
-  case 'l':
-	  //printf("(%3d,%3d)でスペースが押されました\n", x, y);
-	  for (int i = 0; i < MAX_SHOT; i++) {
-		  if (ship[1].myShot[i].isAlive == 0) {
-			  ship[1].myShot[i].isAlive = 1;
-			  ship[1].myShot[i].x = ship[1].x;
-			  ship[1].myShot[i].z = ship[1].z;
-			  ship[1].myShot[i].vx = sin(ship[1].theta / 180.0 * PI);
-			  ship[1].myShot[i].vz = cos(ship[1].theta / 180.0 * PI);
-			  break;
-		  }
-	  }
-
-	  break;
 
   case 'w':
 	  //printf("(%3d,%3d)で[↑]が押されました\n", x, y);
@@ -65,25 +51,6 @@ void keyboard(unsigned char key, int x, int y)
   case 'd':
 	  //printf("(%3d,%3d)で[→]が押されました\n", x, y);
 	  KeyRightON = true;
-	  break;
-
-  case 'r':
-	  for (int j = 0; j<SHIP_NUM; j++) {
-		  // 自機の構造体データを初期化
-		  ship[j].x = rand()%50;	// 自機の初期位置(X座標)
-		  ship[j].z = rand() % 50;	// 自機の初期位置(Z座標)
-		  ship[j].theta = rand() % 360;	// 自機の初期位置(Z座標)
-		  ship[j].Life = DEFAULT_LIFE;	// 自機は生きている
-
-		  // 弾の構造体データを初期化(弾はZ軸方向に毎フレーム-1.0移動する)
-		  for (int i = 0; i < MAX_SHOT; i++) {
-			  ship[j].myShot[i].isAlive = 0; // 弾は発射されていない
-			  ship[j].myShot[i].x = 0;		// 弾の初期位置(X座標)
-			  ship[j].myShot[i].z = 0;		// 弾の初期位置(X座標)
-			  ship[j].myShot[i].vx = 0;	// 弾の基本速度(X座標)
-			  ship[j].myShot[i].vz = -1.0; // 弾の基本速度(X座標)
-		  }
-	  }
 	  break;
   }
 }
